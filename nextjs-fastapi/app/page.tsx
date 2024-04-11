8dff56a9d25e362bca34d44810f91450a37be432
Aiturgan <aiturgan888@gmail.com>
2024-04-11 14:02:54 -0500

    Next + fastapi starter

---
+++ nextjs-fastapi/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Home() {
+	return (
+		<main>
+			<h1>Hello, world, geminidocs here</h1>
+		</main>
+	);
+}
