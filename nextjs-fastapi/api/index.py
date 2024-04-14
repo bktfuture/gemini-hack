08275e1f09fbf1cc046d942e12050cc43ceb9a42
Khai Phan <khaiphn41@gmail.com>
2024-04-14 00:34:16 -0700

    Add FastAPI setup and boilerplate
    packages and basic Gemini API test

--- nextjs-fastapi/api/index.py
+++ nextjs-fastapi/api/index.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv('api/.env')
+
+# Access the Gemini API key
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+genai.configure(api_key=GEMINI_API_KEY)
+model = genai.GenerativeModel('gemini-pro')
 
 app = FastAPI()
 
+
 @app.get("/api/python")
 def hello_world():
-    return {"message": "Hello World"}
+    return {"message": [m.name for m in genai.list_models() if 'generateContent' in m.supported_generation_methods]}
+
+@app.get("/api/test")
+def test():
+    response = model.generate_content("What is FastAPI?")
+    return {"Gemini API test response": response.text}
