6eb651a3cdf01d0465ba2af2441a82cd1998576e
Khai Phan <khaiphn41@gmail.com>
2024-04-18 23:31:58 -0700

    Move nextjs-fastapi folder contents outside

--- api/index.py
+++ api/index.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
 from api.database import engine
 from api.models.user import User
-from api.routers import gemini
 
 app = FastAPI()
-
-app.include_router(gemini.router, prefix="/gemini")
 
 
 @app.get("/api/python")
