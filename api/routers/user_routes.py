d318e3d1899576302714ca4117e677e4a664d5e7
Mathew Chen <62050214+chenmathew@users.noreply.github.com>
2024-04-28 16:19:18 -0700

    requested changes

--- api/routers/user_routes.py
+++ api/routers/user_routes.py
@@ -1,17 +1,12 @@
 from api.database import engine
 from api.models.user import User
+from fastapi import HTTPException
 from . import router
 
 @router.post("/temp-create-user")
 async def init_user(username, first_name, last_name, email):
     user = await engine.find_one(User, User.email == email)
     if user:
-        return {
-            "status": False,
-            "response": 'User already created'
-		}
+        raise HTTPException(status_code=400, detail='user already created')
     await engine.save(User(username=username, first_name=first_name, last_name=last_name, email=email))
-    return {
-        "status": True,
-        "response": "user successfully created"
-    }
+    return { "response": "user successfully created" }
