bde50db31de251863bd3b114471d77017b4090e6
Kyle Joseph <kyle@Kyles-MacBook-Air.local>
2024-04-30 13:31:43 -0700

    Added the big banner component for the dashboard.

---
+++ components/BigBanner/BigBanner.tsx
@@ -0,0 +1,14 @@
+import "./BigBanner.css";
+
+function BigBanner() {
+  return (
+    <div className="container">
+      <p className="upper-text">The Ultimate Student Toolkit.</p>
+      <p className="lower-text">
+        Simplify Applications and Achieve Your Dreams.
+      </p>
+    </div>
+  );
+}
+
+export default BigBanner;
