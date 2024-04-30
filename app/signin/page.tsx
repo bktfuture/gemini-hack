dd6444b00c4ca9be8661758ea6d4a173f8414a2b
Kyle Joseph <kyle@Kyles-MacBook-Air.local>
2024-04-30 14:14:14 -0700

    Centered login properly, but still needs adjustments to account for the layout's body flex.

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -29,40 +29,42 @@ function SignIn() {
   return (
     <div>
       <Navbar />
-      <div className="sign-in-container">
-        <p className="sign-in-text add-30-margin-bottom">Sign In</p>
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
-        <form onSubmit={handleSignIn}>
-          <div className="everything-above-button">
-            <label htmlFor="email">Email</label>
-            <input
-              className="email-input add-30-margin-bottom"
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <label htmlFor="password">Password</label>
-            <input
-              className="password-input add-30-margin-bottom"
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <a href="#" className="forgot-password add-30-margin-bottom">
-              Forgot Password?
-            </a>
-          </div>
-          <div className="everything-button-and-below">
-            <button type="submit">Sign In</button>
-            <p>or</p>
-            <button>Continue with Google</button>
-            <p>Don't have an account yet? Register for free</p>
-          </div>
-        </form>
+      <div className="center-sign-in-container">
+        <div className="sign-in-container">
+          <p className="sign-in-text add-30-margin-bottom">Sign In</p>
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
+          <form onSubmit={handleSignIn}>
+            <div className="everything-above-button">
+              <label htmlFor="email">Email</label>
+              <input
+                className="email-input add-30-margin-bottom"
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                className="password-input add-30-margin-bottom"
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <a href="#" className="forgot-password add-30-margin-bottom">
+                Forgot Password?
+              </a>
+            </div>
+            <div className="everything-button-and-below">
+              <button type="submit">Sign In</button>
+              <p>or</p>
+              <button>Continue with Google</button>
+              <p>Don't have an account yet? Register for free</p>
+            </div>
+          </form>
+        </div>
       </div>
       <Footer />
     </div>
