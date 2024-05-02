db925ab866183a222ace80f6c7f1e0d9c9b0628f
Kyle Joseph <kylejosephswe@gmail.com>
2024-05-01 19:01:16 -0700

    Added tasks component.

---
+++ components/TasksSection/IndividualTask.tsx
@@ -0,0 +1,63 @@
+import "./IndividualTask.css";
+import Image from "next/image";
+import BlackCheckBoxSVG from "../../public/icons/check_box.svg";
+import PinkCheckBoxSVG from "../../public/icons/pink_check_box.svg";
+import BlueCheckBoxSVG from "../../public/icons/blue_check_box.svg";
+import PurpleCheckBoxSVG from "../../public/icons/purple_check_box.svg";
+import BlankCheckBoxSVG from "../../public/icons/blank_check_box.svg";
+import DragIndicatorSVG from "../../public/icons/drag_indicator.svg";
+import { useState } from "react";
+
+interface IndividualTaskProps {
+  text: string;
+}
+
+interface checkBoxMapTypes {
+  [key: number]: string;
+}
+
+function IndividualTask({ text }: IndividualTaskProps) {
+  const [checkbox, setCheckbox] = useState(false);
+  const [randomNumber, setRandomNumber] = useState(1);
+
+  const checkBoxMap: checkBoxMapTypes = {
+    1: BlueCheckBoxSVG,
+    2: PurpleCheckBoxSVG,
+    3: PinkCheckBoxSVG,
+  };
+
+  function toggleCheckbox() {
+    if (checkbox === false) {
+      const generatedRandomNumber: number = Math.floor(Math.random() * 3) + 1;
+      setRandomNumber(generatedRandomNumber);
+      setCheckbox(true);
+    } else {
+      setCheckbox(false);
+    }
+  }
+
+  return (
+    <div className="task">
+      <div className="checkbox-and-task-text">
+        <Image
+          className="checkbox"
+          onClick={toggleCheckbox}
+          src={checkbox ? checkBoxMap[randomNumber] : BlankCheckBoxSVG}
+          alt="checkbox"
+        />
+        <p
+          className={
+            checkbox ? "task-text-black-checkbox" : "task-text-blank-checkbox"
+          }
+        >
+          {text}
+        </p>
+      </div>
+      <div className="drag-indicator-outer-div">
+        <Image src={DragIndicatorSVG} alt="Drag indicator" />
+      </div>
+    </div>
+  );
+}
+
+export default IndividualTask;
---
+++ components/TasksSection/TasksSection.tsx
@@ -0,0 +1,31 @@
+"use client";
+import "./TasksSection.css";
+import Image from "next/image";
+import IndividualTask from "./IndividualTask";
+import CheckBoxSVG from "../../public/icons/check_box.svg";
+import DotDotDotButtonSVG from "../../public/icons/DotDotDotButton.svg";
+
+function Tasks(props: any) {
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="left-header">
+          <Image
+            className="circle-for-checkbox"
+            src={CheckBoxSVG}
+            alt="checkbox"
+          ></Image>
+          <p className="header-text">Tasks</p>
+        </div>
+        <Image src={DotDotDotButtonSVG} alt="dot dot dot button" />
+      </div>
+      <div className="tasks">
+        {Object.keys(props).map((key) => (
+          <IndividualTask text={props[key]} key={key} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Tasks;
