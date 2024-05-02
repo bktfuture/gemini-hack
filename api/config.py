c0b80ae8a444047a389a4b25fac8aeebe717ac45
Preston <hangprestonn@gmail.com>
2024-05-02 00:58:45 -0700

    Added .env gets for client_secret

--- api/config.py
+++ api/config.py
@@ -14,3 +14,15 @@ MONGODB_DB_NAME = os.getenv("DB_NAME")
 genai.configure(api_key=GEMINI_API_KEY)
 text_model = genai.GenerativeModel('gemini-pro')
 image_model = genai.GenerativeModel('gemini-pro-vision')
+
+# Deployment URL
+BASE_URL = os.getenv('BASE_URL')
+
+# Client_secret Access
+CLIENT_ID = os.getenv('CLIENT_ID')
+PROJECT_ID = os.getenv('PROJECT_ID')
+AUTH_URI = os.getenv('AUTH_URI')
+TOKEN_URI = os.getenv('TOKEN_URI')
+AUTH_PROVIDER = os.getenv('AUTH_PROVIDER')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
