17ac999071f0fe03b4bbed3d158380402fa5093b
Aiturgan <aiturgan888@gmail.com>
2024-05-02 22:52:20 -0500

    hero

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import styles from './indexpage.module.css';
 import Navbar from '@/components/Navbar/Navbar';
 import Footer from '@/components/Footer/Footer';
 import college from '../public/collegeApp.svg';
+import fasfa from '../public/fasfa.png';
+import visa from '../public/visa.png';
 
 export default function Home() {
 	return (
@@ -25,13 +27,43 @@ export default function Home() {
 					<Image src={college} width={500}></Image>
 				</div>
 				<div className={styles.col}>
-					<h1>College Applications</h1>
-					<h2 className={styles.hBig}>Secure acceptance from your dream isntitution</h2>
+					<h1>COLLEGE APPLICATIONS</h1>
+					<h2 className={styles.hBig}>Secure acceptance from your dream institution</h2>
 					<p>
 						Gemini Docs simplifies your journey to studying in the US by ensuring you meet admission requirements and guiding you through the
 						specific Visa application required for your chosen institution.
 					</p>
 					<button className={styles.getStarted}>Get started</button>
+				</div>
+			</div>
+			<br />
+			<div className={styles.box}>
+				<div className={styles.col1}>
+					<h1>FASFA</h1>
+					<h2 className={styles.hBig}>Get the financial aid you deserve</h2>
+					<p>
+						Struggling to fill out the FAFSA application? You're not alone. But don't let financial aid paperwork hold you back from your college
+						dreams! Our app makes completing the FAFSA easier than ever. Get step-by-step instructions and make work simple.
+					</p>
+					<button className={styles.getStarted2}>Get started</button>
+				</div>
+				<div className={styles.imgbox}>
+					<Image src={fasfa} width={500}></Image>
+				</div>
+			</div>
+
+			<div className={styles.box3}>
+				<div className={styles.imgbox}>
+					<Image src={visa} width={500}></Image>
+				</div>
+				<div className={styles.col3}>
+					<h1>VISA</h1>
+					<h2 className={styles.hBig}>Enchance your chances of obtaining Visa</h2>
+					<p>
+						The Visa process involves securing acceptance from your desired educational institution, completing necessary financial aid
+						applications, and attending a Visa interview. Gemini Docs helps you fill out documentation & realize your dreams of studying in the US.
+					</p>
+					<button className={styles.getStarted3}>Get started</button>
 				</div>
 			</div>
 			<Footer></Footer>
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -11,36 +11,36 @@ import { useState } from 'react';
 import axios from 'axios';
 
 function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const router = useRouter();
-  const { userInfo, setUserInfo } = useGlobalContext();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [errorMessage, setErrorMessage] = useState('');
+	const router = useRouter();
+	const { userInfo, setUserInfo } = useGlobalContext();
 
-  function handleSignIn(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    axios
-      .get("http://127.0.0.1:8000/api/v1/user/auth-user", {
-        params: {
-          email: email,
-          password: password,
-        },
-      })
-      .then((response) => {
-        setUserInfo("firstName", response.data.user_info.first_name);
-        setUserInfo("lastName", response.data.user_info.last_name);
-        setUserInfo("user_id", response.data.user_info.id);
-        setUserInfo("email", response.data.user_info.email);
-        router.push("/dashboard");
-      })
-      .catch((error) => {
-        setErrorMessage("Please enter your email and password");
-        setUserInfo("email", "");
-        setPassword("");
-        console.error("Error:", error);
-        return;
-      });
-  }
+	function handleSignIn(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		axios
+			.get('http://127.0.0.1:8000/api/v1/user/auth-user', {
+				params: {
+					email: email,
+					password: password,
+				},
+			})
+			.then((response) => {
+				setUserInfo('firstName', response.data.user_info.first_name);
+				setUserInfo('lastName', response.data.user_info.last_name);
+				setUserInfo('user_id', response.data.user_info.id);
+				setUserInfo('email', response.data.user_info.email);
+				router.push('/dashboard');
+			})
+			.catch((error) => {
+				setErrorMessage('Please enter your email and password');
+				setUserInfo('email', '');
+				setPassword('');
+				console.error('Error:', error);
+				return;
+			});
+	}
 
 	function handleGoogleSignIn() {
 		router.push('http://127.0.0.1:8000/api/v1/glogin/login');
@@ -57,58 +57,54 @@ function SignIn() {
 						<div className="everything-above-button">
 							{/* <label className="label" htmlFor="username">
                 Username
-              </label>
-              <label className="label" htmlFor="email">
-                Email
-              </label>
-              <input
-                className="input add-30-margin-bottom"
-                type="email"
-                id="email"
-                placeholder="username@gmail.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-              <label className="label" htmlFor="password">
-                Password
-              </label>
-              <input
-                className="input add-30-margin-bottom"
-                type="password"
-                id="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <a href="#" className="forgot-password add-30-margin-bottom">
-                Forgot Password?
-              </a>
-            </div>
-            <div className="everything-button-and-below">
-              <button className="custom-sign-in-button" type="submit">
-                Sign In
-              </button>
-              <p>or</p>
-              <Image
-                onClick={handleGoogleSignIn}
-                src={GoogleSignIn}
-                alt="Continue with Google button"
-              />
-              <p className="dont-have-account-text">
-                Don't have an account yet?{" "}
-                <Link className="registration-link" href="/createaccount/step1">
-                  Register for free
-                </Link>
-              </p>
-            </div>
-          </form>
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
+              </label> */}
+							<label className="label" htmlFor="email">
+								Email
+							</label>
+							<input
+								className="input add-30-margin-bottom"
+								type="email"
+								id="email"
+								placeholder="username@gmail.com"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+								required
+							/>
+							<label className="label" htmlFor="password">
+								Password
+							</label>
+							<input
+								className="input add-30-margin-bottom"
+								type="password"
+								id="password"
+								placeholder="Password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								required
+							/>
+							<a href="#" className="forgot-password add-30-margin-bottom">
+								Forgot Password?
+							</a>
+						</div>
+						<div className="everything-button-and-below">
+							<button className="custom-sign-in-button" type="submit">
+								Sign In
+							</button>
+							<p>or</p>
+							<Image onClick={handleGoogleSignIn} src={GoogleSignIn} alt="Continue with Google button" />
+							<p className="dont-have-account-text">
+								Don't have an account yet?{' '}
+								<Link className="registration-link" href="/createaccount/step1">
+									Register for free
+								</Link>
+							</p>
+						</div>
+					</form>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default SignIn;
